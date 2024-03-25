5359d892b15d1c4f75b0f15eeda3f1a39da22079
MukterHossain <mukterhossain3075@gmail.com>
2024-03-25 21:37:23 +0600

    added navbar

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../component/Navbar";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h2>Home Page</h2>
+        </div>
+    );
+};
+
+export default Home;
